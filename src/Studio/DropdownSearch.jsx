514db9eaa7a39a96004da092762dafae25dca8f9
AbdganiyuUthmanBonuola <uthmanadedance@gmail.com>
2025-01-16 13:12:32 +0100

    Create new file

---
+++ src/Studio/DropdownSearch.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+const DropdownSearch = ({options}) => {
+
+const [selectedOption, selectedOption] = useState(null);
+const [searchQuery, setSearchQuery] = useState('');
+
+  return (
+    <div>DropdownSearch</div>
+  )
+}
+
+export default DropdownSearch
